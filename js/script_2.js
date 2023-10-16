deed95445024d99f54509fa5dca2a1f5272b74dd
19Urii91 <633mir@mail.ru>
2023-10-16 20:08:20 +0800

    lesson logical operators

--- js/script_2.js
+++ js/script_2.js
@@ -1,28 +1,60 @@
-// const num = 50;
+"use strict";
 
-// if (num < 49) {
-//     console.log("error");
-// } else if (num > 100) {
-//     console.log("Много");
-// } else {
-//     console.log("Ок");
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//   console.log("Я сыт!");
 // }
 
-// (num === 50) ? console.log("Ок") : console.log("error");
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
 
-const num = "51";
+// console.log(hamburger === 3 && cola && fries);
 
-switch (num) {
-    case "49":
-        console.log("Неверно");
-        break;
-    case "100":
-        console.log("Неверно");
-        break;
-    case "50":
-        console.log("В точку");
-        break;
-    default:
-        console.log("Не в этот раз");
-        break;
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "dsvdvdsvds");
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//   console.log("Все сыты");
+// } else {
+//   console.log("Мы уходим!");
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+  console.log("Все довольны");
+} else {
+  console.log("Мы уходим!");
 }
+
+console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);
+
+let jonhReport, alexReport, samReport, mariaReport = "done";
+
+console.log(jonhReport || alexReport || samReport || mariaReport);
+
+console.log(!0);
+
+console.log( NaN || 2 || undefined );
+
+console.log( NaN && 2 && undefined );
+
+console.log( 1 && 2 && 3 );
+
+console.log( !1 && 2 || !3 );
+
+console.log( 25 || null && !3 );
+
+console.log( NaN || null || !3 || undefined || 5);
+
+console.log( NaN || null && !3 && undefined || 5);
+
+console.log( 5 === 5 && 3 > 1 || 5);
