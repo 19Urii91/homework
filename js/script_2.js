756c2f89eed5de62108d49dc44c2c0c50611f592
19Urii91 <633mir@mail.ru>
2023-11-05 17:31:41 +0800

    выполняет практику часть 3, используя functions

--- js/script_2.js
+++ js/script_2.js
@@ -37,7 +37,7 @@ function rememberMyFilms() {
     }
 }
 
-rememberMyFilms();
+// rememberMyFilms();
 
 
 // let i = 0;
@@ -94,6 +94,20 @@ function detectPersonalLevel() {
       }
 }
 
-detectPersonalLevel();
+// detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
